eb5f61b33be58174a45e6dd52a419efc322fa2a8
gregfee-okta <greg.fee@okta.com>
2023-05-21 15:32:33 -0700

    wip

--- setup/start_services.sh
+++ setup/start_services.sh
@@ -4,6 +4,7 @@ minikube start --memory 16000 --cpus 8 --disk-size 80g --driver docker
 eval $(minikube -p minikube docker-env)
 cd hive-metastore-docker && ./build_image.sh && cd ..
 cd trino && ./build_image.sh && cd ..
+cd trino-ranger-demo/ranger-admin && ./build_image.sh && cd ../..
 
 ### Minio Setup
 kubectl apply -f ./minio_dev.yaml
@@ -18,30 +19,54 @@ kubectl apply -f ./metastore.yaml
 
 ### Ranger setup - must start maria first
 kubectl apply -f ./es_deployment.yaml
-kubeclt apply -f ./ranger_admin.yaml
+kubectl apply -f ./ranger_admin.yaml
 
 ### Trino setup
 kubectl apply -f ./trino-ranger-cfgs.yaml
 kubectl apply -f ./trino-ranger.yaml
 
 # Setup port forwards in a different terminal
-cd query_cli
+cd ../query_cli
+virtualenv venv
 . venv/bin/activate
+pip install -r requirements3.txt
 ./query_cli setup-port-forwards
+# (leave this running and open a new terminal)
 
 # Setup hive bucket
 aws s3api create-bucket --bucket hive --endpoint-url http://localhost:9000
 
+# Run all the non-kube services
+# Orgdata service 
+# (new terminal)
+cd ../orgdata
+virtualenv venv
+. venv/bin/activate
+pip install -r requirements3.txt
+python main.py
+
+# taskman service
+# (new terminal)
+cd ../taskman
+virtualenv venv
+. venv/bin/activate
+pip install -r requirements3.txt
+python main.py
+# (new terminal)
+. venv/bin/activate
+rq worker --with-schedule
+
+# query service
+cd ../query
+virtualenv venv
+. venv/bin/activate
+pip install -r requirements3.txt
+python main.py
+
 # Setup roles and users
-cd query_cli
+cd ../query_cli
 . venv/bin/activate
 ./query_cli create-default-users -o org123
 ./query_cli create-default-roles -o org123
 
-# TODO: run all the non-kube services
-
 # SETUP COMPLETE
-
-# Validate setup
-
-./query_cli 

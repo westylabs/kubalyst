9b2724ff50f132d48856b3eea9b6046cf9e3a627
gfee-home <30540945+gfee-home@users.noreply.github.com>
2023-06-02 15:37:34 -0700

    Add schema lifetime tests (#13)

--- query_cli/tests/integration/test_snowflake_connector.py
+++ query_cli/tests/integration/test_snowflake_connector.py
@@ -1,8 +1,7 @@
-
-
+from typing import List, Optional, Any, Tuple
 import snowflake.connector
 
-from tests.integration.conftest import orgdata
+
 
 
 def test_basic_error(all_services):
@@ -40,7 +39,7 @@ def test_basic_ddl(all_services):
 
     con.cursor().execute("create or replace database testdb1")  
     con.cursor().execute("use database testdb1")  
-    con.cursor().execute("create or replace schema test1")
+    con.cursor().execute("create or replace schema test2")
 
 
 def test_basic_fetchall(all_services):
@@ -85,3 +84,88 @@ def test_data_types(all_services):
     ]:
         print(f"executing statement {statement}")
         values = con.cursor().execute(statement).fetchall()
+
+
+
+class Statement():
+    def __init__(
+        self,
+        sql_statement: str,
+        expected_result: Optional[List[List[Any]]] = None,
+        error: Optional[Tuple[str, str, str]] = None
+    ) -> None:
+        assert expected_result is None or error is None
+        self.sql_statement = sql_statement
+        self.expected_results = expected_result
+        self.error = error
+
+
+def _run_test(statements: List[Statement]) -> None:
+    con = snowflake.connector.connect(
+        host="localhost",
+        port=7782,
+        protocol="http",
+        user='dude@sweet.com',
+        password='XXXX',
+        account='org123',
+        session_parameters={
+            'QUERY_TAG': 'EndOfMonthFinancials',
+        }
+    )
+
+    for statement in statements:
+        try:
+            cursor = con.cursor().execute(statement.sql_statement)
+            if statement.expected_results is not None:
+                result = cursor.fetchall()
+                assert len(result) == len(statement.expected_results)
+                for result_row, expected_result_row in zip(result, statement.expected_results):
+                    assert len(result_row) == len(expected_result_row)
+                    for result_field, expected_result_field in zip(result_row, expected_result_row):
+                        assert result_field == expected_result_field
+        except snowflake.connector.errors.ProgrammingError as e:
+            assert statement.error is not None
+            assert e.errno == statement.error[0]
+            assert e.sqlstate == statement.error[1]
+            assert statement.error[2] in e.msg
+
+
+def test_schema_lifetime() -> None:
+    _run_test([
+        Statement(
+            "create or replace database testdb",
+        ),
+        Statement(
+            "use database testdb",
+            expected_result=[["Statement executed successfully."]],
+        ),
+        Statement(
+            "drop schema if exists test1",
+        ),        
+        Statement(
+            "drop schema if exists test1",
+            expected_result=[["Drop statement executed successfully (TEST1 already dropped)."]],
+        ),  
+        Statement(
+            "create schema test1",
+            expected_result=[["Schema TEST1 successfully created."]],
+        ),          
+        Statement(
+            "drop schema if exists test1",
+            expected_result=[["TEST1 successfully dropped."]],
+        ),                
+    ])
+
+    _run_test([
+        Statement(
+            "create or replace database testdb",
+        ),
+        Statement(
+            "use database testdb",
+            expected_result=[["Statement executed successfully."]],
+        ),
+        Statement(
+            "drop if exists schema",
+            error=(1003, "42000", "SQL compilation error:\nsyntax error line 1 at position 5 unexpected 'if'."),
+        ),        
+    ])

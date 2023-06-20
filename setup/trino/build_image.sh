4dd37f8f0f8dd038b70ab156e7939f2209e25ce7
Cerek Hillen <crockeo@users.noreply.github.com>
2023-06-19 21:40:27 -0700

    set up user defined functions (#32)

--- setup/trino/build_image.sh
+++ setup/trino/build_image.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 RANGER_VERSION=2.3.0
 
 if [ ! -d "ranger-${RANGER_VERSION}-trino-plugin" ]
@@ -9,6 +10,12 @@ then
     curl https://github.com/aakashnand/trino-ranger-demo/releases/download/trino-ranger-demo-v1.0/ranger-${RANGER_VERSION}-trino-plugin.tar.gz --output ranger-${RANGER_VERSION}-trino-plugin.tar.gz -L
     tar xvf ranger-${RANGER_VERSION}-trino-plugin.tar.gz
     yes | cp -rf trino-ranger-install.properties ranger-${RANGER_VERSION}-trino-plugin/install.properties
+fi
+
+if [ ! -d "$SCRIPT_DIR/plugins" ]; then
+    pushd "$SCRIPT_DIR/../.." >/dev/null
+    make trino-plugins
+    popd >/dev/null
 fi
 
 REPONAME=gfee

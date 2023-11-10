c8bbf82a6f090f26d234a339ab0cd2021a85d823
Bill Graham <billgraham@gmail.com>
2023-11-10 08:10:28 -0800

    Remove warnings from 3rd party libs, reduce line length duplication

--- test.sh
+++ test.sh
@@ -2,4 +2,6 @@
 
 set -euo pipefail
 
+# TODO: why do we fail with the following when trying to introspect all test method names?
+# AttributeError: 'Testing' object has no attribute '__jsii_ref__'
 pytest k8s/test/test_synth.py::test_synth -s
